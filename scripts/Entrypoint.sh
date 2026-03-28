61697fe8ec02e03f509f1983432a9ec3452fde73
Harry Wyatt <harrywyatt5@outlook.com>
2026-03-28 16:20:21 +0000

    Remove sourcing ros2 packages

--- scripts/Entrypoint.sh
+++ scripts/Entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 source /opt/ros/jazzy/setup.bash
-# If needed, source nitros packages
-if [[ -d /opt/isaac_ros ]]; then
-    source /opt/isaac_ros/install/local_setup.bash
-fi
 export OMNI_KIT_ALLOW_ROOT=1
 export LANG=en_US.UTF-8
 exec "$@"
